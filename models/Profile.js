c82a23d271ec049ed48064e38ddf873dc8c41612
Massimo Ganzerli <massimoganzerli@yahoo.com>
2018-11-04 01:40:26 +0100

    Profile schema

---
+++ models/Profile.js
@@ -0,0 +1,113 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId, //associate the user by its id, need to refer the collection
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  job: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String], //array of type strings
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: Strng
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      environment: {
+        type: String
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  infos: [
+    {
+      school: {
+        type: String
+      },
+      certificates: {
+        type: String
+      },
+      field: {
+        type: String
+      },
+      from: {
+        type: Date
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
